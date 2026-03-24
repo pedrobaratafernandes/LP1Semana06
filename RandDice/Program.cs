3ac0aa97d048762e3e26d129187b265c44f9108f
Pedro Fernandes <pedro.barata.fernandes@gmail.com>
2026-03-24 19:02:05 +0000

    RandDice

---
+++ RandDice/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RandDice
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int n = int.Parse(args[0]);
+            int s = int.Parse(args[1]);
+            Random r1 = new Random(s);
+            int total = 0;
+            for (int i = 0; i < n; i++)
+            {
+                total += r1.Next(1, 7);
+            }
+            Console.Write(total);
+        }
+    }
+}

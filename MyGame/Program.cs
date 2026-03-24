feda4d02bae5ae6d8058faf163ea388982aeff36
Pedro Fernandes <pedro.barata.fernandes@gmail.com>
2026-03-24 19:06:50 +0000

    Exercicio 8

---
+++ MyGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyGame
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP!");
+        }
+    }
+}
